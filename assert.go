aeb0f0b587b77a24f765627ea8590dc0e7380e01
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-08-10 23:28:30 +0200

    fix: display loc info of where must was called
    Before it used to display loc info of must itself.

--- assert.go
+++ assert.go
@@ -12,7 +12,17 @@ func assert(v bool, msg string) {
 	}
 }
 
-func must[T any](t T, err error) T {
-	assert(err == nil, fmt.Sprintf("must: %v", err))
+func mustVal[T any](t T, err error) T {
+	if err != nil {
+		pc, f, l, _ := runtime.Caller(1)
+		panic(fmt.Sprintf("must in %s[%s:%d] failed: %v", runtime.FuncForPC(pc).Name(), f, l, err))
+	}
 	return t
+}
+
+func must(err error) {
+	if err != nil {
+		pc, f, l, _ := runtime.Caller(1)
+		panic(fmt.Sprintf("must in %s[%s:%d] failed: %v", runtime.FuncForPC(pc).Name(), f, l, err))
+	}
 }
