fa39e27f2af3be8b38a8879e2faa7cbd895022b1
Colin van Loo <colin@vanloo.ch>
2023-09-06 15:32:42 +0200

    wip: serve error page using Go's templates

--- cmd/rms_web/main.go
+++ cmd/rms_web/main.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"html/template"
 	"io/fs"
+	"log"
 	"net/http"
 )
 
@@ -13,7 +14,32 @@ var embedFiles embed.FS
 
 var Files fs.FS = embedFiles
 
+//go:embed templates/main.html
+var mainPage string
+
 func main() {
+	var data struct {
+		Title                  string
+		Status, Msg, Desc, URL string
+	}
+	data.Title = "Hello, World!"
+	data.Status = http.StatusText(http.StatusBadRequest)
+	data.Msg = "Permission Denied"
+	data.Desc = "Missing authorization token."
+	data.URL = "https://www.example.com/error?code=401"
+	tmpl, err := template.New("main page").Parse(mainPage)
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Print(err)
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
 func TemplateHandler(fs fs.FS, route string, funcs template.FuncMap) (http.HandlerFunc, error) {
