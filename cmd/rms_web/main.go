3b73cb117b3a602e939aed1f511843a7ed11e6a1
Colin van Loo <colin@vanloo.ch>
2023-11-06 21:49:10 +0100

    not found route

--- cmd/rms_web/main.go
+++ cmd/rms_web/main.go
@@ -30,43 +30,48 @@ type errorData struct {
 }
 
 func main() {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.New("main")
-		tmpl.Funcs(map[string]any{
-			"CallTemplate": func(name string, data any) (html template.HTML, err error) {
-				buf := bytes.NewBuffer([]byte{})
-				err = tmpl.ExecuteTemplate(buf, name, data)
-				html = template.HTML(buf.String())
-				return
-			},
-		})
-
-		{
-			fd := must(templateFiles.Open("templates/main.html"))
-			defer panicIf(fd.Close())
-			bs := must(io.ReadAll(fd))
-			must(tmpl.Parse(string(bs)))
-		}
-
-		{
-			fd := must(templateFiles.Open("templates/error.html"))
-			defer panicIf(fd.Close())
-			bs := must(io.ReadAll(fd))
-			must(tmpl.New("body").Parse(string(bs)))
-		}
-
-		data := map[string]any{
-			"Title": "Permission Denied",
-			"Data": errorData{
-				Status: http.StatusText(http.StatusUnauthorized),
-				Msg:    "Missing authorization token",
-				Desc:   "You need to authenticate in order to use this service.",
-				URL:    "https://www.example.com/error?code=401",
-			},
-		}
-		panicIf(tmpl.Execute(w, data))
-	}
+	http.HandleFunc("/", notFoundHandler)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello, World!"))
+	})
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.New("main")
+	tmpl.Funcs(map[string]any{
+		"CallTemplate": func(name string, data any) (html template.HTML, err error) {
+			buf := bytes.NewBuffer([]byte{})
+			err = tmpl.ExecuteTemplate(buf, name, data)
+			html = template.HTML(buf.String())
+			return
+		},
+	})
+
+	{
+		fd := must(templateFiles.Open("templates/main.html"))
+		defer panicIf(fd.Close())
+		bs := must(io.ReadAll(fd))
+		must(tmpl.Parse(string(bs)))
+	}
+
+	{
+		fd := must(templateFiles.Open("templates/error.html"))
+		defer panicIf(fd.Close())
+		bs := must(io.ReadAll(fd))
+		must(tmpl.New("body").Parse(string(bs)))
+	}
+
+	data := map[string]any{
+		"Title": "Not Found",
+		"Data": errorData{
+			Status: http.StatusText(http.StatusNotFound),
+			Msg:    "Page not found.",
+			Desc:   "The requested page does not exist on the server.",
+			URL:    "https://www.example.com/error?code=404",
+		},
+	}
+	w.WriteHeader(http.StatusNotFound)
+	panicIf(tmpl.Execute(w, data))
 }
